e3ef5b7fc1155fba3bde90db09489a0e900378fa
TrongPhu <phuinest@gmail.com>
2024-04-16 12:27:18 +0700

    add-redux-toolkit-and dockerfile

--- problem2/my-app/src/index.tsx
+++ problem2/my-app/src/index.tsx
@@ -7,6 +7,7 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
+import AppStore from './store/AppStore'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -28,11 +29,13 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
+      <AppStore>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </QueryClientProvider>
+      </AppStore>
     </I18nextProvider>
   </React.StrictMode>
 );
